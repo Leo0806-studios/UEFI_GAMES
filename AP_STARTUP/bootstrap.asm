b77908cba2ca47bbce24cd95a5745a874ab84ece
Leo0806 <leo0806@outlook.de>
2026-02-10 06:51:29 +0100

    boobootstrap update

--- AP_STARTUP/bootstrap.asm
+++ AP_STARTUP/bootstrap.asm
@@ -32,8 +32,58 @@ gdt_end:
 [bits 32]
 align 32
 protectedMode:;yayyyyyy no more real mode :3 :3 :3 :3
-mov ebx, cr0
+    ;here i need to compute the actual values for the pagemap entries. ill do that before anzthing else so i dont clobber any values
 
+
+mov ebx, cr0
+shr ebx, 31
+test ebx, ebx
+jz .paginDisabled
+mov ebx ,cr0
+and ebx, ~(1 << 31)
+mov cr0, ebx
+jmp .paginDisabled
+.paginDisabled:
+xor eax , eax
+or eax, PRESENT
+or eax, 0<<1
+or eax, 0<<2
+or eax, 1<<3
+or eax, 0<<4
+or eax, 0<<5
+or eax, 0<<6
+or eax, 0<<7
+or eax, 0<<8
+or eax, 0<<9
+or eax, 0<<10
+or eax, 0<<11
+mov ebx, pdpt
+shr ebx,12
+shl ebx,12
+or eax, ebx
+mov [pml4+4],eax
+
+
+
+
+ mov edx, cr4
+  or  edx, (1 << 5)
+  mov cr4, edx
+
+
+  mov eax, pml4
+  mov cr3, eax
+
+    or ebx, (1 << 31) | (1 << 0)
+  mov cr0, ebx
+
+  ; Now reload the segment registers (CS, DS, SS, etc.) with the appropriate segment selectors...
+
+  mov ax, 64
+  mov ds, ax
+  mov es, ax
+  mov fs, ax
+  mov gs, ax
 
 ;constants to make page table easier
 PRESENT         equ 1<<0
