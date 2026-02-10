a00b255fccad65147db84a2e58deb9c99920dbc5
Leo0806 <66413517+Leo0806-studios@users.noreply.github.com>
2026-02-10 11:10:40 +0100

    update to bootstrap code

--- AP_STARTUP/bootstrap.asm
+++ AP_STARTUP/bootstrap.asm
@@ -2,89 +2,76 @@
 [org 0x7000]
 align   16
 realModeBootstrap:
-cli
-xor ax, ax
-mov ds, ax
-mov es, ax
-mov ss, ax
-mov ax, StackBottom
-add ax, 4096
-mov sp, ax
-lgdt [gdt_desc] 
-mov eax, cr0
-or eax, 1
-mov cr0, eax
-jmp 0x08:protectedMode
+	cli
+	xor ax, ax
+	mov ds, ax
+	mov es, ax
+	mov ss, ax
+	mov ax, StackBottom
+	add ax, 4096
+	mov sp, ax
+	lgdt [gdt_desc] 
+	mov eax, cr0
+	or eax, 1
+	mov cr0, eax
+	jmp CodeProtectedMode:protectedMode
 StackBottom: times 4096 DB 0x0
 gdt:
-    dq 0                ;null descriptor
-    dq 0x00CF9A000000FFFF ;kernel code 32
-    dq 0x00CF92000000FFFF;kernel data 32
-    dq 0x00AF9A000000FFFF;kernel code 64
-    dq 0x00CF92000000FFFF;kernel data 64
-    dq 0 ;tss
+	dq 0                ;null descriptor
+	dq 0x00CF9A000000FFFF ;kernel code 32
+	dq 0x00CF92000000FFFF;kernel data 32
+	dq 0x00AF9A000000FFFF;kernel code 64
+	dq 0x00CF92000000FFFF;kernel data 64
+	dq 0 ;tss
 
 gdt_desc:
-    dw gdt_end - gdt - 1
-    dd gdt
+	dw gdt_end - gdt - 1
+	dd gdt
 
 gdt_end:
+	CodeProtectedMode:  equ 08
+	DataProtectedMode:  equ 16
+	CodeLongMode:       equ 24
+	DataLongMode:       equ 32
 [bits 32]
 align 32
 protectedMode:;yayyyyyy no more real mode :3 :3 :3 :3
-    ;here i need to compute the actual values for the pagemap entries. ill do that before anzthing else so i dont clobber any values
+	;here i need to compute the actual values for the pagemap entries. ill do that before anzthing else so i dont clobber any values
 
-
-mov ebx, cr0
-shr ebx, 31
-test ebx, ebx
-jz .paginDisabled
-mov ebx ,cr0
-and ebx, ~(1 << 31)
-mov cr0, ebx
-jmp .paginDisabled
-.paginDisabled:
-xor eax , eax
-or eax, PRESENT
-or eax, 0<<1
-or eax, 0<<2
-or eax, 1<<3
-or eax, 0<<4
-or eax, 0<<5
-or eax, 0<<6
-or eax, 0<<7
-or eax, 0<<8
-or eax, 0<<9
-or eax, 0<<10
-or eax, 0<<11
-mov ebx, pdpt
-shr ebx,12
-shl ebx,12
-or eax, ebx
-mov [pml4+4],eax
+	mov ax, DataProtectedMode
+	mov ds, ax
+	mov es, ax
+	mov ss, ax
 
 
 
 
- mov edx, cr4
-  or  edx, (1 << 5)
-  mov cr4, edx
+	mov edx, cr4
+	or  edx, (1 << 5)
+	mov cr4, edx
+; Set LME (long mode enable)
+	mov ecx, 0xC0000080
+	rdmsr
+	or  eax, (1 << 8)
+	wrmsr
 
+;load pageMap
+	mov eax, pml4
+	mov cr3, eax
 
-  mov eax, pml4
-  mov cr3, eax
+;enalble paging
+	mov eax, cr0
+	or eax, (1 << 31) | (1 << 0)
+	mov cr0, eax
 
-    or ebx, (1 << 31) | (1 << 0)
-  mov cr0, ebx
-
-  ; Now reload the segment registers (CS, DS, SS, etc.) with the appropriate segment selectors...
-
-  mov ax, 64
-  mov ds, ax
-  mov es, ax
-  mov fs, ax
-  mov gs, ax
-
+; Now reload the segment registers (CS, DS, SS, etc.) with the appropriate segment selectors...
+	mov ax, DataLongMode
+	mov ds, ax
+	mov es, ax
+	mov fs, ax
+	mov gs, ax
+	mov ss, ax
+	jmp CodeLongMode:longMode
 ;constants to make page table easier
 PRESENT         equ 1<<0
 WRITABLE        equ 1<<1
@@ -92,14 +79,58 @@ PAGESIZE        equ 1<<7 ;sets pagesize to 2 mib
 ;page Table
 align 4096
 pml4:
-    dq  0;need to fill on runtime
-    times 511 dq 0 ;grrr must be one page in size. much wasted space
+	dq  pdpt+ (PRESENT | WRITABLE)
+	times 511 dq 0 ;grrr must be one page in size. much wasted space
+align 4096
 pdpt:
-    dq  0;need to fill on runtime
-    times 511 dq 0 ;grrr must be one page in size. much wasted space
+	dq  pd+ (PRESENT | WRITABLE)
+	times 511 dq 0 ;grrr must be one page in size. much wasted space
+align 4096
 pd:    
-    dq 0x00000000 | PRESENT | WRITABLE | PAGESIZE   ; 0–2 MiB
-    dq 0x00200000 | PRESENT | WRITABLE | PAGESIZE   ; 2–4 MiB
-    dq 0x00400000 | PRESENT | WRITABLE | PAGESIZE   ; 4–6 MiB
-    dq 0x00600000 | PRESENT | WRITABLE | PAGESIZE   ; 6–8 MiB
-    times 508 dq 0
+	dq 0x00000000 | PRESENT | WRITABLE | PAGESIZE   ; 0–2 MiB
+	dq 0x00200000 | PRESENT | WRITABLE | PAGESIZE   ; 2–4 MiB
+	dq 0x00400000 | PRESENT | WRITABLE | PAGESIZE   ; 4–6 MiB
+	dq 0x00600000 | PRESENT | WRITABLE | PAGESIZE   ; 6–8 MiB
+	dq 0x00800000 | PRESENT | WRITABLE | PAGESIZE   ; 8-10
+	dq 0x00A00000 | PRESENT | WRITABLE | PAGESIZE   ; 10-12
+	times 506 dq 0
+
+
+	[bits 64]
+	align 64
+	longMode:;finaly long mode
+		;todo in longmode
+		;   query processor ID
+		;   set reached long mode flag 
+		;loop untill Continiue flag is set
+		;   get stack pointer from array.
+		;   update stack pointer
+		;   jump to apKernelLoop
+;get local core id
+	mov eax, 1        
+	cpuid             
+	shr rbx, 24       
+	mov rax, rbx      
+	DEFAULT ABS
+	mov rbx, qword [PtrReachedLongModeArray]
+.loopWait:
+	test rbx,rbx
+	jz .loopWait
+
+	add rbx, rax
+	mov [rbx],byte 1 ;set Ready Flag
+	mov rcx, [ContiniueFlag]
+	.loopWaitContiniue:
+	test rcx, rcx
+	jz .loopWaitContiniue
+
+	mov rbx, [PtrStackArray]
+	add rbx,rax
+	mov rsp, [rbx]
+	jmp [ApKernelLoop]
+PtrStackArray: dq 0x0; will be filled by the kernel once all processors are ready. is a vodi** indexed by the processor id
+PtrReachedLongModeArray: dq 0x0; will be filled before sipi is send so that processors can signal when they reachedlong mode. is a bool* indexed by the processor id
+ContiniueFlag:              db 0x0; will get set by the kernel once all cores have reached long mode
+ApKernelLoop:               dq 0x0; gets set by the kerenl on load before the sipi is send
+
+
