7607544d27b968079fb8fca19f077bab313c60d1
Leo0806 <66413517+Leo0806-studios@users.noreply.github.com>
2026-02-27 11:59:41 +0100

    forhter work on bcd parsing

--- UEFI_PONG/BOOTLOADER/HEADER/BCD/BCD.cpp
+++ UEFI_PONG/BOOTLOADER/HEADER/BCD/BCD.cpp
@@ -1,22 +1,32 @@
 #include "BCD.h"
 #include "HEAP/HEAP.h"
 #include <stdbool.h>
-
+uint64_t strlenTerm(const char* str, char term) {
+	uint64_t len = 0;
+	for (; str[len] && str[len] != term; len++);
+	return len;
+}
+static void MEMSET(char* src, char val, uint64_t size) {
+	for (uint64_t i = 0; i < size; i++) {
+		src[i] = val;
+	}
+}
 uint64_t StrHash(const char* str)
 {
+#define FNV_OFFSET_BASIS 14695981039346656037ULL
+#define FNV_PRIME        1099511628211ULL
+	uint64_t hash = FNV_OFFSET_BASIS;
 
-	uint64_t len = 0;
-	for (; str[len]; len++);
-	uint64_t seed = 0;
-	uint64_t index = 0;
+	while (*str)
+	{
+		hash ^= (uint8_t)*str;  
+		hash *= FNV_PRIME;      
+		str++;
+	}
+#undef FNV_OFFSET_BASIS
+#undef FNV_PRIME
 
-	for (; index + 8 < len; index += 8) {
-		seed ^= (*((const uint64_t*)&str[index])) + 0x9e3779b9 + (seed << 6) + (seed >> 2);
-	}
-	for (; index < len; index++) {
-		seed ^= ((uint64_t)str[index]) + 0x9e3779b9 + (seed << 6) + (seed >> 2);
-	}
-	return seed;
+	return hash;
 }
 static uint64_t CountSections(const char* str) {
 	uint64_t count = 0;
@@ -57,7 +67,7 @@ return count;
 /// <param name="str"></param>
 /// <param name="CurrentLine"></param>
 /// <returns></returns>
-const char* GetNextLine(const char* str, int64_t CurrentLine) {
+static const char* GetNextLine(const char* str, int64_t CurrentLine) {
 	if (CurrentLine < 0) {
 		return str;
 	}
@@ -74,20 +84,99 @@ const char* GetNextLine(const char* str, int64_t CurrentLine) {
 		}
 	}
 }
+typedef struct {
+	char* str1;
+	char* str2;
+}split;
+split SplitStr(const char* str, char term) {
+	split ret;
+	uint64_t len1 = strlenTerm(str, term);
+	ret.str1 = Alloc(len1 + 1);
+	if (!ret.str1) {
+		ret.str2 = NULL;
+		return ret;
+	}
+	MEMSET(ret.str1, 0, len1 + 1);
+	for (uint64_t i = 0; i < len1; i++) {
+		ret.str1[i] = str[i];
+	}
+	uint64_t len2 = strlenTerm(str,'\0') - len1 - 1;
+	ret.str2 = Alloc(len2 + 1);
+	if (!ret.str2) {
+		DeAlloc(ret.str1);
+		ret.str1 = NULL;
+		return ret;
+	}
+	MEMSET(ret.str2, 0, len2 + 1);
+	for (uint64_t i = 0; i < len2; i++) {
+		ret.str2[i] = str[len1 + 1 + i];
+	}
+	return ret;
+}
 BCD* ParseBCD(const char* bcdStr)
 {
 	const uint64_t countSections = CountSections(bcdStr);
 	const uint64_t CommentCount = CountComments(bcdStr);
 	const uint64_t lines = CountLines(bcdStr);
 	 uint64_t totalIndex = 0;
-	BCD*const  ret = Alloc(sizeof(BCD));
+	BCD*  ret = Alloc(sizeof(BCD));
 	if (!ret) {
 		return NULL;
 	}
+	ret->Sections = Alloc(sizeof(BcdSection) * countSections);
 	int64_t currentLine = -1;
+		BcdSection currentSection = { 0 };
 	while (true) {
 		const char* linePtr = GetNextLine(bcdStr, currentLine);
+		if (!linePtr) {
+			break;
+		}
+		for (uint64_t i = 0;  (linePtr[i]!='\n')&&linePtr[i]; i++) {
+			if (linePtr[i] == '#') {
+				//comment line skip
+				break;
+			}
+			else if (linePtr[i] == '[') {
+				//section start
 
+				ret->Sections[ret->Count] = currentSection;
+				
+				ret->Count++;
+				const uint64_t sectionNameLen = strlenTerm(&linePtr[i + 1], ']');
+				currentSection.Key.str = Alloc(sectionNameLen + 1);
+				if (!currentSection.Key.str) {
+					FreeBCD(ret);
+					return NULL;
+				}
+				MEMSET(currentSection.Key.str, 0, sectionNameLen + 1);
+				for (uint64_t j = 0; j < sectionNameLen; j++) {
+					currentSection.Key.str[j] = linePtr[i + 1 + j];
+				}
+				currentSection.Key.hash = StrHash(currentSection.Key.str);
+			}
+			else {
+				//everything else must by definition be an entry
+				if (!currentSection.Key.str) {
+					//entry without a section is invalid
+					FreeBCD(ret);
+					return NULL;
+				}
+				const split pair = SplitStr(linePtr, ':');
+				if (!pair.str1 || !pair.str2) {
+					//failed to split the line into a key and value
+					if (pair.str1) {
+						DeAlloc(pair.str1);
+					}
+					if (pair.str2) {
+						DeAlloc(pair.str2);
+					}
+					FreeBCD(ret);
+					return NULL;
+				}
+
+			}
+		}
+		currentLine++;
 	}
 
 	return ret;
--- UEFI_PONG/BOOTLOADER/HEADER/BCD/BCD.h
+++ UEFI_PONG/BOOTLOADER/HEADER/BCD/BCD.h
@@ -15,7 +15,7 @@ typedef enum {
 }AllowedValueTypes;
 
 typedef struct {
-	const char* str;
+	 char* str;
 	uint64_t hash;
 }StringHashPair;
 typedef struct {
@@ -42,9 +42,25 @@ uint64_t StrHash(const char* str);
 BCD* ParseBCD(const char* bcdStr);
 
 FreeBCD(BCD* bcd);
+/// <summary>
+/// looks up a entry in a bcd section by its string key. returns a pointer to the pair if found, otherwise returns NULL. the section must be provided as a pointer to avoid having to look it up by string again. if you dont have a pointer to the section use FindPairDirect or FindPairDirectHash instead which will look up the section for you but will be slower.
+/// </summary>
+/// <param name="self"></param>
+/// <param name="section"></param>
+/// <param name="key"></param>
+/// <returns></returns>
 BcdPair* FindPair(BCD* self, BcdSection* section, const char* key);
-BcdPair* FindPairHash(BCD* self, BcdSection* section, uint64_t hash);
+
+/// <summary>
+/// Finds a pair in a BCD section by its hash value.
+/// note that this still itterates in o(n). the hash is just to skipp the string comparsion. the section must be provided as a pointer to avoid having to look it up by string again. if you dont have a pointer to the section use FindPairDirect or FindPairDirectHash instead which will look up the section for you but will be slower.
+/// </summary>
+/// <param name="self">Pointer to the BCD structure to search in.</param>
+/// <param name="section">Pointer to the BCD section containing the pair.</param>
+/// <param name="hash">The hash value to search for.</param>
+/// <returns>Pointer to the found BcdPair, or null if not found.</returns>
+BcdPair* FindPairHash(BCD* self, BcdSection* section, uint64_t hash, const char* fallback);
 BcdPair* FindPairDirect(BCD* self, const char* section, const char* key);
-BcdPair* FindPairDirectHash(BCD* self, uint64_t hashSection, uint64_t hashKey);
+BcdPair* FindPairDirectHash(BCD* self, uint64_t hashSection, const char* fallbackSection, uint64_t hashKey, const char* fallbackKey);
 BcdSection* FindSection(BCD* self, const char* section);
-BcdSection* FindSectionHash(BCD* self, uint64_t hash);
+BcdSection* FindSectionHash(BCD* self, uint64_t hash, const char* fallback);
